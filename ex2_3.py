43c9835d444e07e2a074788903b64cd1acd87957
Mohammad Nadeem <mnadeem.imtiaz@gmail.com>
2022-08-28 23:44:16 +0300

    Area and perimeter of rectangle

---
+++ ex2_3.py
@@ -0,0 +1,8 @@
+length_str = input("Please Enter the length :")
+width_str = input("Please Enter the width :")
+length = float(length_str)
+width = float(width_str)
+perimeter = 2*(length+width)
+area = length*width
+print("Perimeter of rectangle = " + str(perimeter))
+print("Area of rectangle = " + str(area))
