74a8e2afe4d22dd09a9fbbc21eeb9bc14be62998
Mohammad Nadeem <mnadeem.imtiaz@gmail.com>
2022-08-28 23:53:15 +0300

    Sum, product and average of three numbers

---
+++ ex2_4.py
@@ -0,0 +1,12 @@
+first_str = input("Enter first number :")
+second_str = input("Enter second number :")
+third_str = input("Enter third number :")
+first = float(first_str)
+second = float(second_str)
+third = float(third_str)
+sum = first+second+third
+product = first*second*third
+average = (first+second+third)/3
+print("Sum = " + str(sum))
+print("Product = " + str(product))
+print("average = " + str(average))
