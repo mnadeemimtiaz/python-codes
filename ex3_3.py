de4efd610403f1b2ecc5d003fcfee93504852d8b
Mohammad Nadeem <mnadeem.imtiaz@gmail.com>
2022-09-22 00:57:50 +0300

    Hemoglobin Program

--- ex3_3.py
+++ ex3_3.py
@@ -1,10 +1,16 @@
-Class = input("Please Enter the cabin class of cruise ship :")
-if Class == "LUX":
-    print("Upper Deck with a balcony")
-elif Class == "A":
-        print("above the car deck, equipped with a window.")
-elif Class == "B":
-        print("windowless cabin above the car deck.")
-elif Class == "C":
-        print("windowless cabin below the car deck.")
-else: print("Invalid Input")
+gender = str(input("Enter your gender: "))
+
+if gender == "female" or gender == "male":
+    hemo = int(input("Enter your hemoglobin value(g/l): "))
+if (hemo>=117 and hemo<=155 and gender=="female"):
+    print("Your Hemoglobin Value is Normal")
+if (hemo<117 and gender=="female"):
+     print("Your Hemoglobin Value is low")
+if (hemo>155 and gender=="female"):
+     print("Your Hemoglobin Value is high")
+if (hemo>=134 and hemo<=167 and gender=="male"):
+    print("Your Hemoglobin Value is Normal")
+if (hemo<134 and gender=="male"):
+        print("Your Hemoglobin Value is low")
+if (hemo>167 and gender=="male"):
+        print("Your Hemoglobin Value is high")
